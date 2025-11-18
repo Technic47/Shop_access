a25faaf6de18f86cc1586f0509705c6c18355c39
Pavel Kuznetsov <Technic47@gmail.com>
2025-11-18 11:27:27 +0300

    Parameter service added.

--- src/main/java/ru/kuznetsov/shop/business/access/common/ConstValues.java
+++ src/main/java/ru/kuznetsov/shop/business/access/common/ConstValues.java
@@ -11,12 +11,4 @@ public interface ConstValues {
     String ORDER_MODULE = "/order";
     String ORDER_STATUS_MODULE = "/order/status";
     String ORDER_BUCKET_MODULE = "/order/bucket";
-
-    String ADDRESS_PORT = "9080";
-    String PRODUCT_PORT = "9081";
-    String PRODUCT_CATEGORY_PORT = "9082";
-    String STOCK_PORT = "9084";
-    String STORE_PORT = "9083";
-    String OPERATION_PORT = "9085";
-    String ORDER_PORT = "9086";
 }
--- src/main/java/ru/kuznetsov/shop/business/access/config/DataAccessConfig.java
+++ src/main/java/ru/kuznetsov/shop/business/access/config/DataAccessConfig.java
@@ -1,19 +1,27 @@
 package ru.kuznetsov.shop.business.access.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import ru.kuznetsov.shop.parameter.config.ParameterConfig;
+import ru.kuznetsov.shop.parameter.service.ParameterService;
 
-import static ru.kuznetsov.shop.business.access.common.ConstValues.*;
+import static ru.kuznetsov.shop.parameter.common.ParameterKey.*;
 
 @Configuration
+@RequiredArgsConstructor
+@Import(ParameterConfig.class)
 @ComponentScan("ru.kuznetsov.shop.business.access")
 public class DataAccessConfig {
+
+    private final ParameterService parameterService;
 
     @Value("${microservices.baseUrl}")
     private String baseUrl;
@@ -21,46 +29,46 @@ public class DataAccessConfig {
     @Bean
     @Qualifier("address")
     public WebClient getAddressClient() {
-        return getWebClient(ADDRESS_PORT);
+        return getWebClient(parameterService.getParameterValueString(ADDRESS_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("product")
     public WebClient getProductClient() {
-        return getWebClient(PRODUCT_PORT);
+        return getWebClient(parameterService.getParameterValueString(PRODUCT_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("product-category")
     public WebClient getProductCategoryClient() {
-        return getWebClient(PRODUCT_CATEGORY_PORT);
+        return getWebClient(parameterService.getParameterValueString(PRODUCT_CATEGORY_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("stock")
     public WebClient getStockClient() {
-        return getWebClient(STOCK_PORT);
+        return getWebClient(parameterService.getParameterValueString(STOCK_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("store")
     public WebClient getStoreClient() {
-        return getWebClient(STORE_PORT);
+        return getWebClient(parameterService.getParameterValueString(STORE_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("operation")
     public WebClient getOperationClient() {
-        return getWebClient(OPERATION_PORT);
+        return getWebClient(parameterService.getParameterValueString(OPERATION_PORT_PARAMETER));
     }
 
     @Bean
     @Qualifier("order")
     public WebClient getOrderClient() {
-        return getWebClient(ORDER_PORT);
+        return getWebClient(parameterService.getParameterValueString(ORDER_PORT_PARAMETER));
     }
 
-    private WebClient getWebClient(String port){
+    private WebClient getWebClient(String port) {
         return WebClient.builder()
                 .baseUrl(baseUrl + ":" + port)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
